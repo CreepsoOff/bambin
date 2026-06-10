34b0637b5365a448aedf7b4b16787a8cadebb5ee
Alexis Morel <alexismorel.contact@icloud.com>
2026-06-10 16:51:09 +0200

    Add screen activity

---
+++ bambin/Screens/ActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityView.swift
+//  bambin
+//
+//  Created by apprenant130 on 10/06/2026.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityView()
+}
