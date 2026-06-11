bb8251420e436b8d157e55ea283c5969ac68e6a4
CreepsoOff <51055703+CreepsoOff@users.noreply.github.com>
2026-06-11 10:52:29 +0200

    feat: add UserAvatar

---
+++ bambin/Components/UserAvatar.swift
@@ -0,0 +1,24 @@
+//
+//  MyFamilyAvatar.swift
+//  bambin
+//
+//  Created by Apprenant154 on 11/06/2026.
+//
+
+import SwiftUI
+
+struct UserAvatar: View {
+    
+    var user: User
+    
+    var body: some View {
+        Image(user.avatarName)
+            .resizable()
+            .frame(width: 88, height: 88)
+            .clipShape(.circle)
+    }
+}
+
+#Preview {
+    UserAvatar(user: MockData.family[1])
+}
