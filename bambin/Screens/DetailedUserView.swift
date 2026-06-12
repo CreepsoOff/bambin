d361ea664573877f7bffe94c8406270594706231
CreepsoOff <51055703+CreepsoOff@users.noreply.github.com>
2026-06-12 15:19:02 +0200

    chore: ajout d'un écran WIP pour l'écran détaillé d'un membre de la famille

---
+++ bambin/Screens/DetailedUserView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailedUserView.swift
+//  bambin
+//
+//  Created by Apprenant154 on 11/06/2026.
+//
+
+import SwiftUI
+
+struct DetailedUserView: View {
+    
+    var user: User
+    
+    var body: some View {
+        
+        
+        
+    }
+}
+
+#Preview {
+    DetailedUserView(user: MockData.family.users[0])
+}
